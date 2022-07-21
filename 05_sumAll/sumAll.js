df82c48fa7c94ea13b115e6b21191345f7329c9a
Anton <adjatsuk@protonmail.com>
2022-07-22 00:56:56 +0100

    With the help of stackoverflow I've figured out how to pass all but one test where an error is returned if inputs a or b are arrays. Going to look further!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // Check if isNaN
+    if(isNaN(a) || isNaN(b)) return 'ERROR';
+
+    // Check if inputs are less than 0
+    if(a < 0 || b < 0) return 'ERROR'
+
+    //Define min & max integers - this helps if large number is first
+    let min = Math.min(a, b)
+    let max = Math.max(a, b)
+
+    // Using array to store all numbers
+    let sumArr = [];
+    
+    // Pushes each number into the array
+    for(i = min; i < max +1; i++) {
+        sumArr.push(i);
+    }
+
+    // Sum of all numbers in the array
+    let sum = 0;
+    for (let i = 0; i < sumArr.length; i++) {
+        sum += sumArr[i];
+    }
+  
+return sum;
 
 };
 

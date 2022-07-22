52b68c53adaf6862e8a46833cb2a419d35600bde
Anton <adjatsuk@protonmail.com>
2022-07-22 01:04:54 +0100

    Found out that you can use Number.isInteger to check if a number is integer. Exercise now solved!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,9 @@
 const sumAll = function(a, b) {
-    // Check if isNaN
-    if(isNaN(a) || isNaN(b)) return 'ERROR';
-
     // Check if inputs are less than 0
     if(a < 0 || b < 0) return 'ERROR'
+
+    // Check if inputs are integers
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR'
 
     //Define min & max integers - this helps if large number is first
     let min = Math.min(a, b)

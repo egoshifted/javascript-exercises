a68c3b213d44943f850f8689cee4ef70d11b7bd8
Anton <adjatsuk@protonmail.com>
2022-07-22 21:57:09 +0100

    This was the easiest one to figure out. However it seems converting strings to integers can cause problems? I will research this.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+let tempC = (tempF - 32) * 5/9;
+if (tempC % 1 != 0) {
+  return Number(tempC.toFixed(1));
+} else return tempC;
 };
 
-const ctof = function() {
+const ctof = function(tempC) {
+let tempF = (tempC * 9/5) + 32;
+if (tempF % 1 != 0) {
+  return Number(tempF.toFixed(1));
+} else return tempF;
 
 };
 
